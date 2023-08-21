7fe6d39a91f03810e0b3d1502685639e6069d998
Benito Perdomo <benito.perdomo.v@gmail.com>
2023-08-21 17:04:37 -0600

    add mongoose service

---
+++ src/common/services/mongoose.service.ts
@@ -0,0 +1,40 @@
+import mongoose from 'mongoose';
+import debug from 'debug';
+import config from '../../config';
+
+const { host, user, pass, port, database } = config.oauth_mongodb;
+const log: debug.IDebugger = debug('app:mongoose-service');
+
+class MongooseService {
+    private count = 0;
+    private url = `mongodb://${user}:${pass}@${host}:${port}/?authMechanism=DEFAULT&authSource=${database}`;
+
+    private mongooseOptions = {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        serverSelectionTimeoutMS: 5000,
+    };
+
+    constructor() {
+        this.connectWithRetry();
+    }
+
+    getMongoose() {
+        return mongoose;
+    }
+
+    connectWithRetry = async () => {
+        log('Intentando conectar con MongoDB (se reintentará de ser necesario)');
+        mongoose
+            .connect(this.url, this.mongooseOptions)
+            .then(() => {
+                log('conectado a MongoDB');
+            })
+            .catch((err) => {
+                const retrySeconds = 5;
+                log(`Conexión a MongoDB no satisfactoria (reintento #${++this.count} despues ${retrySeconds} segundos):`, err);
+                setTimeout(this.connectWithRetry, retrySeconds * 1000);
+            });
+    };
+}
+export default new MongooseService();
