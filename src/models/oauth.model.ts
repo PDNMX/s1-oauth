8dad835ef69e231bbf1e97b33ea3e54988ef23ea
Benito Perdomo <benito.perdomo.v@gmail.com>
2023-08-31 18:12:24 -0600

    update managment errors and fix type scope value

--- src/models/oauth.model.ts
+++ src/models/oauth.model.ts
@@ -1,4 +1,5 @@
 import mongooseService from "../common/services/mongoose.service";
+import { UnauthorizedError } from "../exceptions/unauthorizedError";
 const mongoose = mongooseService.getMongoose();
 const Schema = mongoose.Schema;
 
@@ -15,7 +16,7 @@ const oauthSchema = new Schema({
     grant_type: String,
     username: String,
     password: String,
-    scope: String,
+    scope: Array,
     client_secret: String,
     client_id: String
 });
@@ -33,7 +34,11 @@ class Oauth {
 
     static check_credentials_exist = async (oauth: IOauth) => {
         const { username, password, client_secret, client_id } = oauth;
-        return this.model.findOne({ username, password, client_id, client_secret }).exec();
+        try {
+            return await this.model.findOne({ username, password, client_id, client_secret }).exec();
+        } catch (error: object | any) {
+            throw new UnauthorizedError('e_4006', 'Proceso de autenticación fallido', error.message);
+        }
     }
 }
 
